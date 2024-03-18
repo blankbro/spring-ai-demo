c6ac7cf1d1f89d5e27cacb93ac9f97ec3e6d3307
lizexin <zexin-li@outlook.com>
2024-03-18 19:40:06 +0800

    test

---
+++ src/main/java/io/github/blankbro/springaidemo/controller/TestController.java
@@ -0,0 +1,11 @@
+package io.github.blankbro.springaidemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+
+}
